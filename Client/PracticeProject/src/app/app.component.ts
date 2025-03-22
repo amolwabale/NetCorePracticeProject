54e7f20d570f148de081d153a40dc63c9d75bad6
amolwabale <a.m.wabale@gmail.com>
2025-03-22 13:31:28 +0530

    adding client side project

---
+++ Client/PracticeProject/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './Component/login/login.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [LoginComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'PracticeProject';
+}
